6a0365227b412a477f8d281dfa7632a8b9ccca45
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2019-12-03 12:28:53 -0700

    Add the exercise for today

---
+++ 2019_12_03/exercise_1.js
@@ -0,0 +1,9 @@
+// Create a program that does the following:
+//
+// 1. Defines and initializes a variable to store your age - in years
+// 2. Defines and initializes a variable to store the number of seconds in a day
+//
+// 3. Calculate the following and output it to the console:
+//     - Your age in seconds
+const some_value = 1;
+console.log(`My age in seconds is ${some_value}`);
