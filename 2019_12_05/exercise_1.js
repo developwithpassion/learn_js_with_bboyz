1283898d6535042c2eb73d052dc30b2c6b88e218
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2019-12-05 11:11:53 -0700

    Added exercise for the day

---
+++ 2019_12_05/exercise_1.js
@@ -0,0 +1,10 @@
+/* 
+ * Write a program that displays to the console the relevant values in a  year
+ * - Use this as an opportunity to demonstrate use of good variable naming
+ * - Use variables to store relevant info that will help you calculate the value
+ * - Make sure that you actually calculate the relevant values
+ * - Make sure the output looks like the following:
+ *
+ *   The number of seconds in a year is: [seconds]
+ *   The number of minutes in a year is: [minutes]
+ *   The number of hours in a year is: [hours]
