a89b4a691df8dfa9610a50895c662602c2763a34
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2019-12-06 13:20:40 -0700

    Updated

---
+++ 2019_12_06/exercise_1.js
@@ -0,0 +1,30 @@
+/*
+* CODING CHALLENGE 3
+
+
+
+John and his family went on a holiday and went to 3 different restaurants. The bills were $124, $48 and $268.
+To tip the waiter a fair amount, John created a simple tip calculator (as a function). He likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+In the end, John would like to have 2 arrays:
+1) Containing all three tips (one for each bill)
+2) Containing all three final paid amounts (bill + tip).
+(NOTE: To calculate 20% of a value, simply multiply it with 20/100 = 0.2)
+GOOD LUCK 😀
+*/
+
+function tipCalculator(bill) {
+  //if the bill is less than 50
+  // return bill * 0.20
+  //if the bill is between 50 and 200
+  // return bill * 0.15
+  // return bill * 0.10
+}
+
+console.log(tipCalculator(124));
+// 18.60
+
+console.log(tipCalculator(48));
+// 9.60
+
+console.log(tipCalculator(268));
+// 26.80
